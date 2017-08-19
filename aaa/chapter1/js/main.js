7852df887ec5793692ab1da44bd8e0251fdad2b6
Louis <Louis@admindeMac-mini.local>
2017-08-19 13:40:29 +0800

    修改文件

--- aaa/chapter1/js/main.js
+++ aaa/chapter1/js/main.js
@@ -373,15 +373,15 @@ var animPages = [];
 //                  ""
 //                  ];//json的地址
 
-var paths = [    "chapter1/t/page2/data.json",
-                 "chapter1/t/page3/data.json",
-                 "chapter1/t/page4/data.json",
+var paths = [    "t/page2/data.json",
+                 "t/page3/data.json",
+                 "t/page4/data.json",
                  "",
-                 "chapter1/c/clutchPage1/data.json",
-                 "chapter1/c/clutchPage2/data.json",
-                 "chapter1/c/clutchPage3/data.json",
-                 "chapter1/c/clutchPage4/data.json",
-                 "chapter1/c/clutchPage5/data.json",
+                 "c/clutchPage1/data.json",
+                 "c/clutchPage2/data.json",
+                 "c/clutchPage3/data.json",
+                 "c/clutchPage4/data.json",
+                 "c/clutchPage5/data.json",
                  ""
                  ];//json的地址
 
